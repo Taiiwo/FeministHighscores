22b9652396fbbac0e48064bfac0d4b5fb252ba18
Taiiwo Llort <tylerzacc@gmail.com>
2014-11-23 17:31:54 +0000

    Insert script description header

---
+++ feministBlocker.py
@@ -0,0 +1,13 @@
+#   / __\__ _ __ ___ (_)_ __ (_)___| |_  / __\ | ___   ___| | _____ _ __ 
+#  / _\/ _ \ '_ ` _ \| | '_ \| / __| __|/__\// |/ _ \ / __| |/ / _ \ '__|
+# / / |  __/ | | | | | | | | | \__ \ |_/ \/  \ | (_) | (__|   <  __/ |   
+# \/   \___|_| |_| |_|_|_| |_|_|___/\__\_____/_|\___/ \___|_|\_\___|_|   
+
+# This is a project inspired by GGAutoBlocker that aims to identify radical
+# feminists and anti-gamergate supporters, and instead of maliciously and
+# oppressively adding them to an ambiguous blocklist conflictingly making
+# them more likely to attempt the behaviour and opinions they are being
+# punished for, this script attempts to allow it's users to identify and
+# aggregate these radicals for the purpose of mass ridicule and
+# identification of such humorously idiotic thoughts.
+
